8e297d59cfb1a8d0af0f88a7b835c4469175a651
vsh51 <volshp125@gmail.com>
2024-08-26 14:52:08 +0300

    SMTP commands implementation

--- crates/console_client/src/main.rs
+++ crates/console_client/src/main.rs
@@ -4,7 +4,17 @@ use smtp_session::SmtpSession;
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut client = SmtpSession::connect("smtp.gmail.com:587").await.unwrap();
-    client.initialize_secure_conncetion().await.unwrap();
+    
+    client.send_ehlo_cmd().await.unwrap();
+    print!("{}", client.read_response().await.unwrap());
+
+    client.send_starttls_cmd().await.unwrap();
+    print!("{}", client.read_response().await.unwrap());
+
+    client.encrypt_connection().await.unwrap();
+
+    client.send_ehlo_cmd().await.unwrap();
+    print!("{}", client.read_response().await.unwrap());
     
     Ok(())
 }
--- crates/smtp_session/src/lib.rs
+++ crates/smtp_session/src/lib.rs
@@ -5,13 +5,23 @@ use SmtpCommand::*;
 pub enum SmtpCommand {
     Ehlo,
     StartTls,
+    AuthPlain,
+    MailFrom,
+    RcptTo,
+    Data,
+    Quit,
 }
 
 impl SmtpCommand {
-    fn get(cmd: Self) -> String {
+    fn to_str(cmd: Self) -> String {
         match cmd {
             Self::Ehlo => "EHLO".to_string(),
             Self::StartTls => "STARTTLS".to_string(),
+            Self::AuthPlain => "AUTH PLAIN".to_string(),
+            Self::MailFrom => "MAIL FROM".to_string(),
+            Self::RcptTo => "RCPT TO".to_string(),
+            Self::Data => "DATA".to_string(),
+            Self::Quit => "QUIT".to_string(),
         }
     }
 }
@@ -30,34 +40,34 @@ impl SmtpSession {
         )
     }
 
-    pub async fn initialize_secure_conncetion(&mut self) -> Result<(), Error> {
-        print!("{}", self.read_response().await?);
+    pub async fn send_ehlo_cmd(&mut self) -> Result<usize, Error> {
+        let command = SmtpCommand::to_str(Ehlo) + " host \r\n";
+        self.m_stream.write(command.as_bytes()).await
+    }
 
-        self.send_ehlo().await?;
-        print!("{}", self.read_response().await?);
-
-        self.send_starttls().await?;
-        print!("{}", self.read_response().await?);
-
+    pub async fn encrypt_connection(&mut self) -> Result<bool, Error> {
         self.m_stream.try_upgrade_to_tls().await?;
-
-        self.send_ehlo().await?;
-        print!("{}", self.read_response().await?);
-
-        Ok(())
+        Ok(true)
     }
 
-    async fn send_ehlo(&mut self) -> Result<usize, Error> {
-        let command = SmtpCommand::get(Ehlo) + " host \r\n";
-        self.m_stream.write(command.as_bytes()).await
+    pub async fn send_starttls_cmd(&mut self) -> Result<usize, Error> {
+        self.send_cmd(StartTls).await
     }
 
-    async fn send_starttls(&mut self) -> Result<usize, Error> {
-        let command = SmtpCommand::get(StartTls) + "\r\n";
-        self.m_stream.write(command.as_bytes()).await
+    pub async fn send_data_cmd(&mut self) -> Result<usize, Error> {
+        self.send_cmd(Data).await
+    }
+
+    pub async fn send_quit_cmd(&mut self) -> Result<usize, Error> {
+        self.send_cmd(Quit).await
     }
 
     pub async fn read_response(&mut self) -> Result<String, Error> {
         self.m_stream.read().await
+    }
+
+    async fn send_cmd(&mut self, cmd: SmtpCommand) -> Result<usize, Error> {
+        let command = SmtpCommand::to_str(cmd) + "\r\n";
+        self.m_stream.write(command.as_bytes()).await
     }
 }
